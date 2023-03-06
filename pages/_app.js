0bb90f719abf86b68df71c02390c231b1efb7607
crochetcode <fred.davison@outlook.com>
2023-03-05 19:33:38 -0600

    install daisyui

--- pages/_app.js
+++ pages/_app.js
@@ -2,10 +2,13 @@ import '../styles/globals.css';
 
 function MyApp() {
     return (
-        <div className={'h-screen flex justify-center items-center'}>
-            <h1 className='text-5xl font-bold'>
-                Hello world!
-            </h1>
+        <div className='hero min-h-screen bg-base-200'>
+            <div className='hero-content text-center'>
+                <div className='max-w-md'>
+                    <h1 className='text-5xl font-bold'>Fred Davison</h1>
+                    <h2 className='text-2xl py-1'>full-stack developer</h2>
+                </div>
+            </div>
         </div>
     );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,6 @@ module.exports = {
     ],    theme: {
         extend: {},
     },
-    plugins: [],
+    plugins: [require('daisyui')],
     purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-
 }
