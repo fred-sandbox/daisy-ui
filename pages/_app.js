1ca29c7607f8024d77db9b37aa69dd27362c448c
crochetcode <fred.davison@outlook.com>
2023-03-05 19:26:03 -0600

    install tailwinds

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp() {
+    return (
+        <div className={'h-screen flex justify-center items-center'}>
+            <h1 className='text-5xl font-bold'>
+                Hello world!
+            </h1>
+        </div>
+    );
 }
 
-export default MyApp
+export default MyApp;
